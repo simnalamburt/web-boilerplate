9c54f570233a44446309ded66f37c38b4b1c3397
Hyeon Kim <simnalamburt@gmail.com>
2015-01-07 14:09:07 +0900

    client: angular-route

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,13 @@
 require('angular');
+require('angular-route');
+
 require('./main.css');
 
-angular.module('appname', []);
+angular.module('appname', ['ngRoute'])
+
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'src/main.html',
+  });
+})
